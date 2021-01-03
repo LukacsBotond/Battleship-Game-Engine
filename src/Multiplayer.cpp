83d66f01d721d21f09d2e48ccc02874008100caf
KristofLevente <72783028+KristofLevente@users.noreply.github.com>
2021-01-03 17:08:38 +0200

    Add files via upload

---
+++ src/Multiplayer.cpp
@@ -0,0 +1,213 @@
+﻿#include "Multiplayer.h"
+
+
+void InitializeWinsock()
+{
+	WSADATA wsaData;
+	int result;
+
+	result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (result != NO_ERROR)
+	{
+		cout << "Hiba a WSAStartup() –nál\n";
+	}
+	//cout << "initialize test" << endl;
+}
+
+/*void stringToCharArray(string s, string s1, char* char_array)
+{
+	string fullString = s + "." + s1;
+
+	//char* char_array;
+
+	char_array = &fullString[0];
+
+	//cout << char_array;
+	//return char_array;
+
+}*/
+
+string CharArrayToString(char* array)
+{
+	string s(array);
+
+	return s;
+}
+
+
+string recvFromPlayer(SOCKET PlayerSocket, sockaddr_in Address)
+{
+	const int messageLen = 25;
+	char message[messageLen];
+	int result;
+	int addressSize = sizeof(Address);
+	string coordinates;
+
+	cout << "waiting for message" << endl;
+	result = recvfrom(PlayerSocket, message, messageLen, 0, (SOCKADDR*)&Address, &addressSize);
+
+	if (result == SOCKET_ERROR)
+	{
+		cout << "Hiba a fogadasnal a kovetkezo hibakoddal:\n" << WSAGetLastError();
+		closesocket(PlayerSocket);
+		return "";
+	}
+
+	message[result] = '\0';
+
+	coordinates = CharArrayToString(message);
+
+	//cout << "Received message:" << coordinates;
+
+	if (result == 0)
+	{
+		cout << "A kapcsolat megszakadt.\n";
+		return "";
+	}
+
+	return coordinates;
+}
+
+sockaddr_in createAddress()
+{
+	sockaddr_in Address;
+
+	Address.sin_family = AF_INET;
+	Address.sin_port = htons(13000);
+
+	inet_pton(AF_INET, "127.0.0.1", &Address.sin_addr);
+
+	return Address;
+
+}
+
+SOCKET bindPlayers(sockaddr_in Address)
+{
+	SOCKET playerSocket;
+
+	playerSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	// Bind the socket.
+	if (bind(playerSocket, (SOCKADDR*)&Address, sizeof(Address)) == SOCKET_ERROR)
+	{
+		printf("bind() failed.\n");
+		cout << WSAGetLastError();
+
+		closesocket(playerSocket);
+		WSACleanup();
+	}
+
+	return playerSocket;
+
+}
+
+void sendToPlayer(sockaddr_in Address, string x, string y)
+{
+
+
+	int addressSize = sizeof(Address);
+	int result;
+	string fullString = x + "." + y;
+
+
+	char* message = new char[fullString.length() + 1];
+
+	strcpy_s(message, fullString.length() + 1, fullString.c_str());
+
+	const int messageLen = strlen(message);
+
+	SOCKET playerSocket;
+
+	playerSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+
+	result = sendto(playerSocket, message, messageLen, 0, (SOCKADDR*)&Address, addressSize);
+	if (result == SOCKET_ERROR)
+	{
+		cout << "Hiba a kuldesnel a kovetkezo hibakoddal:\n" << WSAGetLastError();
+		closesocket(playerSocket);
+		WSACleanup();
+	}
+
+	if (result == 0)
+	{
+		cout << "A kapcsolat megszakadt.\n";
+		closesocket(playerSocket);
+		WSACleanup();
+	}
+
+}
+
+void sendSettingToPlayer(sockaddr_in Address, string setting)
+{
+	int addressSize = sizeof(Address);
+	int result;
+
+	char* message = new char[setting.length() + 1];
+
+	strcpy_s(message, setting.length() + 1, setting.c_str());
+
+	const int messageLen = strlen(message);
+
+	SOCKET playerSocket;
+
+	playerSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+
+	result = sendto(playerSocket, message, messageLen, 0, (SOCKADDR*)&Address, addressSize);
+	if (result == SOCKET_ERROR)
+	{
+		cout << "Hiba a kuldesnel a kovetkezo hibakoddal:\n" << WSAGetLastError();
+		closesocket(playerSocket);
+		WSACleanup();
+	}
+
+	if (result == 0)
+	{
+		cout << "A kapcsolat megszakadt.\n";
+		closesocket(playerSocket);
+		WSACleanup();
+	}
+
+}
+
+void sendMessage(string x, string y)
+{
+	sockaddr_in Address;
+
+	InitializeWinsock();
+	Address = createAddress();
+
+	sendToPlayer(Address, x, y);
+
+	WSACleanup();
+}
+
+void sendSetting(string x)
+{
+	sockaddr_in Address;
+
+	InitializeWinsock();
+	Address = createAddress();
+
+	sendSettingToPlayer(Address, x);
+
+	WSACleanup();
+}
+
+string recvMessage()
+{
+	InitializeWinsock();
+	sockaddr_in Address;
+	SOCKET player;
+	string coordinates;
+
+	Address = createAddress();
+	player = bindPlayers(Address);
+
+	coordinates = recvFromPlayer(player, Address);
+
+	WSACleanup();
+
+	return coordinates;
+}
+
--- src/mainGame.cpp
+++ src/mainGame.cpp
@@ -3,6 +3,7 @@
 #include "AI.h"
 #include "ColorChange.h"
 #include "gameHeader.h"
+#include "Multiplayer.h"
 #include <fstream>
 #include <vector>
 #include <cmath>
@@ -21,7 +22,7 @@ bool checkNumber(string x)
     return true;
 }
 
-int playerShootAI(BattleMap &Player, AI &ai)
+int playerShootAI(BattleMap& Player, AI& ai)
 {
     cout << "PlayerShoot\n";
     cout << endl;
@@ -41,61 +42,113 @@ int playerShootAI(BattleMap &Player, AI &ai)
         }
         //shoot ai
         ret = Player.Shoot(stoi(x), stoi(y), &ai);
-    } while (ret == 1 || ret == 3);
+    } while (ret == 2);
     return ret;
 }
 
 //loop till the game ends
 //NOT DONE
-void MainLoop(BattleMap &Player,int HOST)
+void MainLoop(BattleMap& Player, int HOST)
 {
-    printcolor("Game started", color_orange);
-    int ret;
+    printcolor("Game  rted", color_orange);
+    int ret, i = 0;
     bool IShoot = false;
     //HOST get the firts shot
-    string x,y;
-    if(HOST==1){
-        IShoot=true;
+    string x, y, message, temp,hitOrMiss,game;
+    string coordinate[2];
+    if (HOST == 1) {
+        IShoot = true;
     }
 
     while (true)
     {
         //shooter runs in this part
-        if(IShoot){
+        if (IShoot) {
             cout << "Type in the coordinates for target" << endl;
             cin >> x >> y;
             //TODO send the coordinates to the other side
-
+            sendMessage(x, y);
             //TODO wait for recv is hit/miss won
 
+            game = recvMessage();
+            if (game == "You won")
+            {
+                printcolor("YOU WON", color_orange);
+                EndGame();
+            }
+
+            hitOrMiss = recvMessage();
+            if (hitOrMiss == "Hit")
+            {
+                IShoot = !IShoot;
+            }
             //TODO SET own enemy map accordingly to result
+            Player.printMap(true);
+           // Player.printMap(false);
             //TODO increase hit/miss accordilngly
 
             //TODO if won
             //EndGame();
         }//the other is in this part
-        else{
+        else {
             //TODO recv coordinates
 
+            message = recvMessage();
+            //cout << message;
+            stringstream coord(message);
+
+            while (getline(coord, temp, '.'))
+            {
+                coordinate[i] = temp;  // koordinatak kinyerese az uzenetbol
+                i++;
+            }
+
             //ret = Player.Shoot(4,4);
+            ret = Player.Shoot(stoi(coordinate[0]), stoi(coordinate[1]));
 
-            //TODO return hit/miss/won
+            if (ret == 3)
+            {
+                printcolor("OPONENT WON", color_orange);
+                sendSetting("You won");
+                EndGame();
+            }
+            else
+            {
+                sendSetting("Still playing");
+                Sleep(10);
+            }
 
-            //TODO if won
-            //EndGame();
+            if (ret == 1)
+            {
+                sendSetting("Hit");
+                IShoot = !IShoot;
+            }
+            else
+            {
+                sendSetting("Miss");
+            }
+
+            Player.printMap(true);
+            // Player.printMap(false);
+
+             //TODO return hit/miss/won
+
+             //TODO if won
+             //EndGame();
         }
         //Change roles
-        IShoot= !IShoot;
+        IShoot = !IShoot;
+        i = 0;
     }
     EndGame();
 }
 
-void MainLoopAI(BattleMap &Player, AI &ai)
+void MainLoopAI(BattleMap& Player, AI& ai)
 {
     BattleMap* Player_p;
     AI* ai_p;
-    Player_p=&Player;
-    ai_p=&ai;
+    Player_p = &Player;
+    ai_p = &ai;
     printcolor("Game started", color_orange);
     int ret;
     while (true)
@@ -104,7 +157,7 @@ void MainLoopAI(BattleMap &Player, AI &ai)
         if (ret == 3) //no ship remains
         {
             printcolor("PLAYER WON", color_orange);
-            cout <<(float)Player_p->getRatio()*100<< "%" << endl;
+            cout << (float)Player_p->getRatio() * 100 << "%" << endl;
             EndGame();
         }
         //AI shoot
@@ -130,7 +183,7 @@ void EndGame()
     {
         cout << "type 'settings' to go back and change settings if you are the host" << endl;
         cout << "type 'start' to start with the same settings"
-             << "but you need to set the ships again" << endl;
+            << "but you need to set the ships again" << endl;
         cin >> where;
         if (where == "settings")
         {
